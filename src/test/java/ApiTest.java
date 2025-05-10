68305b1c5ca00afb2d201ee79cc36f70da01225f
Sergey Liah <sergeyliah@ukr.net>
2025-05-10 13:17:23 +0300

    commit2

--- src/test/java/ApiTest.java
+++ src/test/java/ApiTest.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
 
 public class ApiTest {
+    @Test
+    public void testGetUserDetails_UsingAssertableResponse() {
+        RestAssured.baseURI = "https://api.privatbank.ua/p24api";
+        int exchange = 5;
+        RestAssured
+                .given()
+                .basePath("/pubinfo")
+                .param("exchange&coursid", exchange)
+                .when()
+                .get()
+                .then().statusCode(200);
+    }
 }
