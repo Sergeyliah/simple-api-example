1a63dff85375fefcb61b1e47c2a546fd7d6cea01
Sergey Liah <sergeyliah@ukr.net>
2025-05-17 17:11:53 +0300

    add slf4j and logback

--- src/test/java/ApiTest.java
+++ src/test/java/ApiTest.java
@@ -1,15 +1,21 @@
 import io.restassured.RestAssured;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import io.qameta.allure.restassured.AllureRestAssured;
+import java.util.Arrays;
 
+@Slf4j
 public class ApiTest {
     @Test
     public void testGetUserDetails_UsingAssertableResponse() {
+        log.info("Test started");
         RestAssured.baseURI = "https://api.privatbank.ua/p24api";
         int exchange = 5;
         RestAssured
                 .given()
-                .filter(new AllureRestAssured())
+                .filters(Arrays.asList(new RequestLoggingFilter(), new ResponseLoggingFilter(), new AllureRestAssured()))
                 .basePath("/pubinfo")
                 .param("exchange&coursid", exchange)
                 .when()
